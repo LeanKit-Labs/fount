123c42ec2d276ec0f9d21b41c4ed202a359b9ad2
Jim Cowart <jim@ifandelse.com>
2015-03-01 22:50:38 -0500

    Adding istanbul for test coverage. Updated README with how to run tests & coverage, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,36 @@
-var gulp = require( 'gulp' ),
-	mocha = require( 'gulp-mocha' );
+var gulp = require( 'gulp' );
+var mocha = require( 'gulp-mocha' );
+var istanbul = require( 'gulp-istanbul' );
+var open = require( 'open' );
+var allSrcFiles = './src/**/*.js';
+var allSpecFiles = './spec/*.spec.js';
 
 gulp.task( 'test', function() {
-	gulp.src( './spec/*.spec.js' )
+	gulp.src( allSpecFiles )
 		.pipe( mocha( { reporter: 'spec' } ) )
-		.on( 'error', function( err ) { console.log( err.stack ); } );
+		.on( 'error', function( err ) {
+			console.log( err.stack );
+		} );
+} );
+
+gulp.task( 'coverage', function( cb ) {
+	return gulp.src( [ allSrcFiles ] )
+		.pipe( istanbul() )
+		.pipe( istanbul.hookRequire() )
+		.on( 'finish', function() {
+			gulp.src( [ allSpecFiles ] )
+				.pipe( mocha() )
+				.pipe( istanbul.writeReports() );
+		} );
+} );
+
+gulp.task( 'show-coverage', [ 'coverage' ], function( cb ) {
+	open( './coverage/lcov-report/index.html' );
+	cb();
 } );
 
 gulp.task( 'watch', function() {
-	gulp.watch( [ './src/**', './spec/**' ], [ 'test' ] );
+	gulp.watch( [ allSrcFiles, './spec/**' ], [ 'test' ] );
 } );
 
-gulp.task( 'default', [ 'test', 'watch' ], function() {
-} );
+gulp.task( 'default', [ 'test', 'watch' ], function() {} );
